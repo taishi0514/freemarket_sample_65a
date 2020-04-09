381a8469d23409501ecf9fbb8798a1a40a7ab7ae
taishi0514 <58291249+taishi0514@users.noreply.github.com>
2020-04-09 19:53:54 +0900

    追加しました

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,7 @@
 server '18.178.36.63', user: 'ec2-user', roles: %w{app db web}
+
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
