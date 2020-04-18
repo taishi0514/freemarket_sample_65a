553a6464f66808288190234e070d58677ca89b07
tamaiseiya-github <tamaiphone7@gmail.com>
2020-04-18 19:57:36 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -130,7 +130,6 @@ ActiveRecord::Schema.define(version: 2020_04_18_050414) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
@@ -145,7 +144,6 @@ ActiveRecord::Schema.define(version: 2020_04_18_050414) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
