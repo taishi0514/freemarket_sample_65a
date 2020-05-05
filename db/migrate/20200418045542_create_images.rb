545047c7a85c79462d02b0bc685d36285ffbd26f
tamaiseiya-github <tamaiphone7@gmail.com>
2020-05-05 12:39:35 +0900

    5/5玉井（コード調整）

--- db/migrate/20200418045542_create_images.rb
+++ db/migrate/20200418045542_create_images.rb
@@ -1,9 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.2]
   def change
     create_table :images do |t|
-      t.references :products
+      t.references :product, foreign_key: true
       t.string :src
-
       t.timestamps
     end
   end
