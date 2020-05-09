37911798a4924c20fb0b812b0040f741e7a35ec6
taishi0514 <58291249+taishi0514@users.noreply.github.com>
2020-05-09 20:53:00 +0900

    変更

--- db/migrate/20200414232144_create_products.rb
+++ db/migrate/20200414232144_create_products.rb
@@ -2,15 +2,15 @@ class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
       # t.references :user  
-      t.string  :title              
-      t.string  :detail             
-      t.references :category       
-      t.string  :brand             
-      t.string  :condition          
-      t.references  :shippingway	  
-      t.references  :product_size	  
-      t.references  :area  
-      t.string :ship_period  
+      t.string  :title     
+      t.string  :detail    
+      t.references :category
+      t.string  :brand       
+      t.string  :condition    
+      t.references  :shippingway
+      t.references  :product_size
+      t.references  :area
+      t.string :ship_period
       t.integer :price
       t.timestamps
     end
