2ccf2ca031a98989fd449a4f6383cd567326a47c
yoppytaro <yoppygood1201@docomo.ne.jp>
2020-05-13 09:30:59 +0900

    購入ページ修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,12 @@
 class HomeController < ApplicationController
+  before_action :login?, only: [:show]
   def show
     @user = User.find(params[:id])
+  end
+
+  def login?
+    if current_user == nil
+      redirect_to("/users/sign_in")
+    end
   end
 end
--- app/models/product_size.rb
+++ app/models/product_size.rb
@@ -4,17 +4,6 @@ class ProductSize < ApplicationRecord
   has_many :categories, through: :category_sizes
   has_ancestry  
 
-  # バリレーション
 
-  # ユーザーID・地域ID・カテゴリーID・発送方法ID 空でない事 数字である事
-  validates :user_id,:area_id,:category_id,:shippingway_id, presence: true,numericality: true
-  # タイトル・ブランド 空でない事 200文字以内
-  validates :title,:brand, presence: true, length: {maximum: 200}
-  # 説明 空でない事 1000文字以内
-  validates :detail, presence: true, length: {maximum: 1000}
-  # コンディション 空でない事 
-  validates :condition, presence: true
-  # 価格 1000以上10000000以下or等しく 空でない事
-  validates :price,presence: true ,numericality: {greater_than_or_equal_to: 1000,less_than:10000000}
 end
 
